b0e1e4ec1efd723bb54b85d2e13a829158e03ab0
Jose Narvaez <goyox86@gmail.com>
2011-08-24 15:17:07 -0430

    - Removed debug info   - Now the behavior is to raise Resque::RetryLimitReachedWithoutSuccess.new with the original exception class name and message and also the backtrace is set to the original exception backtrace.   - Removed the call to respond_to?(:retry_limit_reached?) since I know that I can respond to that. Unnecessary overhead.

--- lib/resque/plugins/retry_with_success_detection.rb
+++ lib/resque/plugins/retry_with_success_detection.rb
@@ -4,10 +4,12 @@ module Resque
       include Resque::Plugins::Retry
 
       def on_failure_notify(e, *args)
-        puts "I failed! #{self.inspect} on retry_attempt: #{retry_attempt}"
-        if respond_to?(:retry_limit_reached?) && retry_limit_reached?
+        if retry_limit_reached?
           clear_retry_count(*args)
-          raise Resque::RetryLimitReachedWithoutSuccess.new "Job #{self.inspect}:#{self.object_id} has finished it's retry stategy without any success."
+          retry_limit_reached_without_success = Resque::RetryLimitReachedWithoutSuccess.new "Job #{self.inspect}-#{self.object_id} has reached it's maximum retry_limit without any success.
+                                                                                            Original Exception: #{e.class.to_s}-#{e.to_s}"
+          retry_limit_reached_without_success.set_backtrace(e.backtrace)
+          raise retry_limit_reached_without_success
         end
       end
     private
